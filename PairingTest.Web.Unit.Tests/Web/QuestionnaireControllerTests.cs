2b803bcd3d6aefe85a4e0fb1ab5faca418e04702
jikpas0 <jikpas0@hotmail.com>
2017-05-25 21:01:57 +0100

    WIP: Implemented unit tests

---
+++ PairingTest.Web.Unit.Tests/Web/QuestionnaireControllerTests.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+using PairingTest.Web.Controllers;
+using PairingTest.Web.Models;
+
+namespace PairingTest.Unit.Tests.Web
+{
+    [TestFixture]
+    public class QuestionnaireControllerTests
+    {
+        
+    }
+}
